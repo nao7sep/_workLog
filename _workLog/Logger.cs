c8afe48a77abb09292b118b3482729e2bd96d736
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-09-02 17:00:17 +0900

    Add Logger

---
+++ _workLog/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace _workLog
+{
+    public class Logger
+    {
+        public DateTime CreatedAtUtc { get; init; }
+
+        public string Path { get; init; }
+
+        public List <string> Messages { get; } = new ();
+
+        public Logger ()
+        {
+            CreatedAtUtc = DateTime.UtcNow;
+            Path = Environment.MapPath (@$"Logs\{CreatedAtUtc:yyyyMMdd'T'HHmmss'Z'}.log");
+        }
+
+        public void LogAndDisplay (string message, bool flush = false)
+        {
+            Messages.Add (message);
+
+            if (flush)
+                Flush ();
+
+            Console.WriteLine (message);
+        }
+
+        public void Flush ()
+        {
+            Directory.CreateDirectory (Environment.MapPath ("Logs"));
+            File.AppendAllLines (Path, Messages, Encoding.UTF8);
+            Messages.Clear ();
+        }
+
+        private static Logger? _default;
+
+        public static Logger Default => _default ??= new ();
+    }
+}
--- _workLog/Program.cs
+++ _workLog/Program.cs
@@ -4,16 +4,18 @@ class Program
 {
     static void Main (string [] args)
     {
-        // todo
-
-        foreach (string xArg in args)
+        try
         {
-            Attachment xAttachment = Environment.Attach (Guid.NewGuid (), xArg);
-            xAttachment.DisplayAllInfo ();
         }
 
-        Console.WriteLine ("Press any key to exit: ");
-        Console.ReadKey (true);
-        Console.WriteLine ();
+        catch (Exception xException)
+        {
+            Logger.Default.LogAndDisplay (xException.ToString ());
+        }
+
+        finally
+        {
+            Logger.Default.Flush ();
+        }
     }
 }
